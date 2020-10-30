603a3280867490db3bde93573f9b709bd338db26
Ken Mueller <kenmueller0@gmail.com>
2020-10-30 16:11:08 -0700

    api

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,41 @@
+import express from 'express'
 import { createServer } from 'http'
 import IO from 'socket.io'
 
 import User from './models/User'
+import { lines } from './models/Line'
+import { messages } from './models/Message'
 
 const port = process.env.PORT ?? 5000
-const server = createServer((_req, res) => {
-	res.writeHead(301, { Location: 'https://draw.place' })
-	res.end()
+const app = express()
+const http = createServer(app)
+
+app.get('/', (_req, res) => {
+	res.redirect(301, 'https://draw.place')
 })
 
-IO(server).on('connect', io => new User(io))
+app.post('/clear-lines', (req, res) => {
+	if (req.query.pass !== process.env.PASSWORD) {
+		res.status(403).send('Incorrect password')
+		return
+	}
+	
+	lines.splice(0, lines.length)
+	res.send('Cleared lines')
+})
 
-server.listen(port, () => {
+app.post('/clear-messages', (req, res) => {
+	if (req.query.pass !== process.env.PASSWORD) {
+		res.status(403).send('Incorrect password')
+		return
+	}
+	
+	messages.splice(0, messages.length)
+	res.send('Cleared messages')
+})
+
+IO(http).on('connect', io => new User(io))
+
+http.listen(port, () => {
 	console.log(`Listening on http://localhost:${port}`)
 })
