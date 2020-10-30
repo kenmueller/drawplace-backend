74498dcbb44ba8015fb2aed23c1d673d83a9cfe4
Ken Mueller <kenmueller0@gmail.com>
2020-10-29 19:22:02 -0700

    instantiates user

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
 import { createServer } from 'http'
 import IO from 'socket.io'
 
+import User from './models/User'
+
 const http = createServer((_req, res) => res.end())
 const io = IO(http)
 
-io.on('connect', socket => console.log('connect'))
+io.on('connect', socket => new User(socket))
 
 ;(async () => {
 	const port = process.env.PORT ?? 5000
