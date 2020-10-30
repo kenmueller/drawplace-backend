87c3d5ded759ffc4a8d99227db68d2a91e2058d9
Ken Mueller <kenmueller0@gmail.com>
2020-10-29 23:02:26 -0700

    draws lines

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,22 +1,32 @@
 import { Socket } from 'socket.io'
 
-import Cursor from './Cursor'
+import Coordinate from './Coordinate'
+import Line from './Line'
 
 let users: User[] = []
+const lines: Line[] = []
 
 export default class User {
-	private cursor?: Cursor
+	private cursor?: Coordinate
 	
 	constructor(private socket: Socket) {
 		users.push(this)
 		
 		this.emitOtherCursors()
+		this.emitLines()
 		
-		socket.on('cursor', (cursor: Cursor) => {
+		socket.on('cursor', (cursor: Coordinate) => {
 			this.cursor = cursor
 			
 			for (const user of this.otherUsers)
 				user.emitOtherCursors()
+		})
+		
+		socket.on('line', (line: Line) => {
+			lines.push(line)
+			
+			for (const user of this.otherUsers)
+				user.emitLine(line)
 		})
 		
 		socket.on('disconnect', () => {
@@ -29,12 +39,21 @@ export default class User {
 		return users.filter(user => user !== this)
 	}
 	
+	private get otherCursors() {
+		return this.otherUsers.reduce((cursors: Coordinate[], { cursor }) => (
+			cursor ? [...cursors, cursor] : cursors
+		), [])
+	}
+	
 	private emitOtherCursors = () => {
-		this.socket.emit(
-			'cursors',
-			this.otherUsers.reduce((cursors: Cursor[], { cursor }) => (
-				cursor ? [...cursors, cursor] : cursors
-			), [])
-		)
+		this.socket.emit('cursors', this.otherCursors)
+	}
+	
+	private emitLines = () => {
+		this.socket.emit('lines', lines)
+	}
+	
+	private emitLine = (line: Line) => {
+		this.socket.emit('line', line)
 	}
 }
