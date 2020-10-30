98d1e9a0dc11333c7fd1e17a4a4cc25c67a42a3f
Ken Mueller <kenmueller0@gmail.com>
2020-10-30 01:31:54 -0700

    color user

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,59 +1,93 @@
 import { Socket } from 'socket.io'
+import generateName from '../generateName'
 
+import UserJson from './UserJson'
 import Coordinate from './Coordinate'
-import Line from './Line'
+import Line, { lines } from './Line'
 
 let users: User[] = []
-const lines: Line[] = []
 
 export default class User {
 	private cursor?: Coordinate
+	private name: string = generateName()
+	private color: string = '#000000'
+	private message?: string
 	
-	constructor(private socket: Socket) {
+	constructor(private io: Socket) {
 		users.push(this)
 		
-		this.emitOtherCursors()
+		this.emitName()
+		this.emitOtherUsers()
 		this.emitLines()
 		
-		socket.on('cursor', (cursor: Coordinate) => {
+		io.on('name', (name: string) => {
+			this.name = name
+			
+			for (const user of this.otherUsers)
+				user.emitOtherUsers()
+		})
+		
+		io.on('color', (color: string) => {
+			this.color = color
+			
+			for (const user of this.otherUsers)
+				user.emitOtherUsers()
+		})
+		
+		io.on('cursor', (cursor: Coordinate) => {
 			this.cursor = cursor
 			
 			for (const user of this.otherUsers)
-				user.emitOtherCursors()
+				user.emitOtherUsers()
 		})
 		
-		socket.on('line', (line: Line) => {
+		io.on('line', (line: Line) => {
 			lines.push(line)
 			
 			for (const user of this.otherUsers)
 				user.emitLine(line)
 		})
 		
-		socket.on('disconnect', () => {
+		io.on('disconnect', () => {
 			for (const user of users = this.otherUsers)
-				user.emitOtherCursors()
+				user.emitOtherUsers()
 		})
+	}
+	
+	private get json(): UserJson | null {
+		return this.cursor
+			? {
+				cursor: this.cursor,
+				name: this.name,
+				color: this.color,
+				message: this.message ?? null
+			}
+			: null
 	}
 	
 	private get otherUsers() {
 		return users.filter(user => user !== this)
 	}
 	
-	private get otherCursors() {
-		return this.otherUsers.reduce((cursors: Coordinate[], { cursor }) => (
-			cursor ? [...cursors, cursor] : cursors
+	private get jsonOtherUsers() {
+		return this.otherUsers.reduce((users: UserJson[], { json }) => (
+			json ? [...users, json] : users
 		), [])
 	}
 	
-	private emitOtherCursors = () => {
-		this.socket.emit('cursors', this.otherCursors)
+	private emitName = () => {
+		this.io.emit('name', this.name)
+	}
+	
+	private emitOtherUsers = () => {
+		this.io.emit('users', this.jsonOtherUsers)
 	}
 	
 	private emitLines = () => {
-		this.socket.emit('lines', lines)
+		this.io.emit('lines', lines)
 	}
 	
 	private emitLine = (line: Line) => {
-		this.socket.emit('line', line)
+		this.io.emit('line', line)
 	}
 }
