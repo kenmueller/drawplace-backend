630f398c8e80f54c1b7329588f4b78d409e6434c
Ken Mueller <kenmueller0@gmail.com>
2020-10-30 22:50:27 -0700

    fixed messages

--- src/models/Message.ts
+++ src/models/Message.ts
@@ -1,5 +1,6 @@
 import admin from 'firebase-admin'
 
+const { FieldValue } = admin.firestore
 const firestore = admin.firestore()
 
 export interface UserMessage {
@@ -23,10 +24,18 @@ type Message = UserMessage | JoinMessage | LeaveMessage
 
 export default Message
 
-export const getMessages = async (limit: number = 100) =>
-	(await firestore.collection('messages').limit(limit).get())
+export const getMessages = async (limit: number = 50) =>
+	(await firestore.collection('messages').orderBy('date', 'desc').limit(limit).get())
 		.docs
-		.map(snapshot => snapshot.data() as Message)
+		.map(snapshot => {
+			const data = snapshot.data()
+			delete data.date // Only used for ordering
+			return data as Message
+		})
+		.reverse()
 
 export const addMessage = async (message: Message) =>
-	(await firestore.collection('messages').add(message)).id
+	(await firestore
+		.collection('messages')
+		.add({ ...message, date: FieldValue.serverTimestamp() })
+	).id
