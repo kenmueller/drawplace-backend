b5a1ed22f5c3bac05948d3da3a39403c0ee7bc84
Ken Mueller <kenmueller0@gmail.com>
2020-11-06 15:35:42 -0800

    clear api

--- src/index.ts
+++ src/index.ts
@@ -11,6 +11,9 @@ admin.initializeApp({
 })
 
 import User from './models/User'
+import Bounds, { areBoundsNaN } from './models/Bounds'
+import { getChunksInBounds, getChunkId } from './models/Chunk'
+import clearLinesInBounds from './utils/clearLinesInBounds'
 
 const port = process.env.PORT ?? 5000
 const origin = process.env.NODE_ENV === 'production'
@@ -27,6 +30,32 @@ app.use((_req, res, next) => {
 
 app.get('/', (_req, res) => {
 	res.redirect(301, 'https://draw.place')
+})
+
+app.post('/clear/:x1/:y1/:x2/:y2', async ({
+	params: { x1, y1, x2, y2 },
+	query: { pass }
+}, res) => {
+	if (pass !== process.env.PASSWORD) {
+		res.status(401).send('Incorrect password')
+		return
+	}
+	
+	const bounds: Bounds = {
+		lower: { x: parseInt(x1, 10), y: parseInt(y1, 10) },
+		upper: { x: parseInt(x2, 10), y: parseInt(y2, 10) }
+	}
+	
+	if (areBoundsNaN(bounds)) {
+		res.status(400).send('Invalid coordinates')
+		return
+	}
+	
+	await Promise.all(getChunksInBounds(bounds).map(coordinate =>
+		clearLinesInBounds(getChunkId(coordinate), bounds)
+	))
+	
+	res.send(`Cleared lines between ${bounds.lower.x}/${bounds.lower.y} and ${bounds.upper.x}/${bounds.upper.y}`)
 })
 
 IO(http).on('connect', io => new User(io))
