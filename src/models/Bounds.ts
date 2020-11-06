e8e4f7b9179dbec3ce621f9da8cd047c1308865d
Ken Mueller <kenmueller0@gmail.com>
2020-11-06 15:35:32 -0800

    are bounds nan

--- src/models/Bounds.ts
+++ src/models/Bounds.ts
@@ -4,3 +4,7 @@ export default interface Bounds {
 	lower: Coordinate
 	upper: Coordinate
 }
+
+export const areBoundsNaN = ({ lower, upper }: Bounds) =>
+	Number.isNaN(lower.x) || Number.isNaN(lower.y) ||
+	Number.isNaN(upper.x) || Number.isNaN(upper.y)
